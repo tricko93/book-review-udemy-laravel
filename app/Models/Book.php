a8a0e1db74e820b7ca9c0448d1224aebc0f0d183
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-11-25 16:15:40 +0100

    feature Add date range filter for books
    This commit:
    
    - Adds an optional date range filter to the scopePopular and
      scopeHighestRated functions in Book.php.
    
    - Allows sorting books by popularity or highest average rating within a
      specified date range.
    
    - Helps users to find the most relevant books for their interests and
      preferences.
    
    Example usage:
    
    - \App\Models\Book::popular('2021-01-01', '2021-12-31')->get() to
      retrieve popular books published within a specific date range.
    
    - \App\Models\Book::highestRated('2021-01-01', '2021-12-31')->get() to
      retrieve the highest-rated books published within a specific date
      range.
    
    The commit also adds comments for all the functions in the Book class,
    explaining their parameters, return values, and logic.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,30 +5,80 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
     use HasFactory;
 
+    /**
+     * Add a foreign key in the reviews table to reference the ID in the books table.
+     */
     public function reviews()
     {
         return $this->hasMany(Review::class);
     }
 
+    /**
+     * Scope to search books by title.
+     *
+     * @param  Builder  $query
+     * @param  string  $title
+     * @return Builder
+     */
     public function scopeTitle(Builder $query, string $title): Builder
     {
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query): Builder
+    /**
+     * Scope to retrieve books sorted by popularity.
+     *
+     * @param  Builder  $query
+     * @param  mixed|null  $from
+     * @param  mixed|null  $to
+     * @return Builder|QueryBuilder
+     */
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withCount('reviews')
+        return $query->withCount([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
             ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder
+    /**
+     * Scope to retrieve books sorted by average rating.
+     *
+     * @param  Builder  $query
+     * @param  mixed|null  $from
+     * @param  mixed|null  $to
+     * @return Builder|QueryBuilder
+     */
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withAvg('reviews', 'rating')
+        return $query->withAvg([
+            'reviews' => fn (Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
             ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    /**
+     * Filter books by the start date and end date.
+     *
+     * @param  Builder  $query
+     * @param  mixed|null  $from
+     * @param  mixed|null  $to
+     * @return Builder|QueryBuilder
+     */
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
+    {
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
