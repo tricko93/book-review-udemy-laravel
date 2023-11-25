29cf1a84386669b21ebbf612053c40a67ad4ff37
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-11-25 22:17:13 +0100

    feature Add logic for filter options in index view
    Refactored BookController's index method to correctly implement the new
    scope methods for filtering books based on popularity and ratings.
    
    Replaced conditional statements with a match expression for cleaner code
    logic.

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -93,4 +93,64 @@ class Book extends Model
             $query->whereBetween('created_at', [$from, $to]);
         }
     }
+
+    /**
+     * Scope that retrieves books with the most reviews in the last month
+     * along with their average ratings and books with at least 2 reviews.
+     *
+     * @param  Builder  $query
+     * @return Builder|QueryBuilder
+     */
+    public function scopePopularLastMonth(Builder $query): 
+    Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    /**
+     * Retrieves books with the most reviews in the last 6 months
+     * along with their average ratings and books with at least 5 reviews.
+     *
+     * @param  Builder  $query
+     * @return Builder|QueryBuilder
+     */
+    public function scopePopularLast6Months(Builder $query): 
+    Builder|QueryBuilder
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    /**
+     * Retrieves books with the average ratings in the last month
+     * along with their popularity ratings and books with at least 2 reviews.
+     *
+     * @param  Builder  $query
+     * @return Builder|QueryBuilder
+     */
+    public function scopeHighestRatedLastMonth(Builder $query): 
+    Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    /**
+     * Retrieves books with the average ratings in the last 6 months
+     * along with their popularity ratings and books with at least 5 reviews.
+     *
+     * @param  Builder  $query
+     * @return Builder|QueryBuilder
+     */
+    public function scopeHighestRatedLast6Months(Builder $query): 
+    Builder|QueryBuilder
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
 }
