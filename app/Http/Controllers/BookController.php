0eefa0734281f62460b547043bcf5abe0c9cc506
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-11-28 22:36:43 +0100

    feature Add pagination for books
    - Update index.blade.php to display links for navigating pages of tasks.
    
    - Update BookController to use paginate(10) instead of get() for
      fetching books from the database.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -33,7 +33,7 @@ class BookController extends Controller
             // $cacheKey,
             // 3600,
             // fn() =>
-            $books->get();
+            $books->paginate(10);
         // );
 
         return view('books.index', ['books' => $books]);
