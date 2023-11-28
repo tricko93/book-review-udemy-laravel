62d5d0084bb26afcf029fcb59fbd33b709e67275
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-11-28 20:24:34 +0100

    feature Add caching: BookController, Review model
    - In the 'BookController@index' method, the retrieved books are cached
      for 3600 seconds (1 hour) with a key based on the provided filter and
      title parameters.
    
    - In the 'BookController@show' method, the book retrieval process has
      been modified to incorporate caching. The book details, along with the
      latest reviews, are now loaded from the cache if available. Otherwise,
      they are fetched from the database and stored in the cache with the
      book's ID as the key.
    
    - In the 'Review' model, the 'booted' method has been overridden to
      handle cache invalidation. Whenever a review is updated or deleted,
      the corresponding book cache entry is cleared using
      'cache()->forget('book:' . $review->book_id)'.

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,7 +26,9 @@ class BookController extends Controller
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
             default => $books->latest()
         };
-        $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn() => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
@@ -52,14 +54,13 @@ class BookController extends Controller
      */
     public function show(Book $book)
     {
-        return view(
-            'books.show',
-            [
-                'book' => $book->load([
-                    'reviews' => fn ($query) => $query->latest()
-                ])
-            ]
-        );
+        $cacheKey = 'book:' . $book->id;
+
+        $book = cache()->remember($cacheKey, 3600, fn() => $book->load([
+            'reviews' => fn ($query) => $query->latest()
+        ]));
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
