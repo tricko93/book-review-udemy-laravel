9c8383c287a7b7669bbace749e7203827e0228ab
Marko Trickovic <96424684+tricko93@users.noreply.github.com>
2023-11-30 20:35:02 +0100

    feature Show flash messages for review validation
    This commit shows success and error flash messages in the main layout
    file (app.blade.php) when users add book reviews. The messages inform
    users if their review passed or failed the validation.
    
    The ReviewController@store method flashes the messages based on the
    validation result.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,65 +5,95 @@
     <meta charset="UTF-8">
     <title>Book Reviews</title>
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 
     {{-- blade-formatter-disable --}}
     <style type="text/tailwindcss">
         .btn {
           @apply bg-white rounded-md px-4 py-2 text-center font-medium text-slate-500 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50 h-10;
-        }
+      }
 
-        .input {
+      .input {
           @apply shadow-sm appearance-none border w-full py-2 px-3 text-slate-700 leading-tight focus:outline-none rounded-md border-slate-300;
-        }
+      }
 
-        .filter-container {
+      .filter-container {
           @apply mb-4 flex space-x-2 rounded-md bg-slate-100 p-2;
-        }
+      }
 
-        .filter-item {
+      .filter-item {
           @apply flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium text-slate-500;
-        }
+      }
 
-        .filter-item-active {
+      .filter-item-active {
           @apply bg-white shadow-sm text-slate-800 flex w-full items-center justify-center rounded-md px-4 py-2 text-center text-sm font-medium;
-        }
+      }
 
-        .book-item {
+      .book-item {
           @apply text-sm rounded-md bg-white p-4 leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
-        }
+      }
 
-        .book-title {
+      .book-title {
           @apply text-lg font-semibold text-slate-800 hover:text-slate-600;
-        }
+      }
 
-        .book-author {
+      .book-author {
           @apply block text-slate-600;
-        }
+      }
 
-        .book-rating {
+      .book-rating {
           @apply text-sm font-medium text-slate-700;
-        }
+      }
 
-        .book-review-count {
+      .book-review-count {
           @apply text-xs text-slate-500;
-        }
+      }
 
-        .empty-book-item {
+      .empty-book-item {
           @apply text-sm rounded-md bg-white py-10 px-4 text-center leading-6 text-slate-900 shadow-md shadow-black/5 ring-1 ring-slate-700/10;
-        }
+      }
 
-        .empty-text {
+      .empty-text {
           @apply font-medium text-slate-500;
-        }
+      }
 
-        .reset-link {
+      .reset-link {
           @apply text-slate-500 underline;
-        }
-    </style>
-    {{-- blade-formatter-enable --}}
+      }
+  </style>
+  {{-- blade-formatter-enable --}}
 </head>
 
 <body class="container mx-auto mt-10 mb-10 max-w-3xl">
+    <div x-data="{ flash: true }">
+        @if (session()->has('success'))
+            <div x-show="flash"
+                class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700" role="alert">
+                <strong class="font-bold">Success!</strong>
+                <div>{{ session('success') }}</div>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" @click="flash = false"
+                        stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
+        @elseif (session()->has('error'))
+            <div x-show="flash"
+                class="relative mb-10 rounded border border-red-400 bg-red-100 px-4 py-3 text-lg text-red-700" role="alert">
+                <strong class="font-bold">Error!</strong>
+                <div>{{ session('error') }}</div>
+                <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                        stroke-width="1.5" @click="flash = false"
+                        stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </span>
+            </div>
+        @endif
+    </div>
     @yield('content')
 </body>
 
